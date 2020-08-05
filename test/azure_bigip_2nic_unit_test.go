8efc87140a93b531b8e73637ba3a678274389e1f
sgaddam <s.gaddam@f5.com>
2020-08-06 02:31:26 +0530

    adding test folder

--- test/azure_bigip_2nic_unit_test.go
+++ test/azure_bigip_2nic_unit_test.go
@@ -3,12 +3,7 @@ package test
 import (
 	"testing"
 
-	//            "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
-
-	//            "github.com/gruntwork-io/terratest/modules/azure"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	//	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAzureExample(t *testing.T) {
@@ -16,8 +11,6 @@ func TestTerraformAzureExample(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := &terraform.Options{
-
-		// The path to where our Terraform code is located
 
 		TerraformDir: "../example/bigip_azure_2nic_deploy",
 
@@ -29,14 +22,5 @@ func TestTerraformAzureExample(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
-
-	//      vmName := terraform.Output(t, terraformOptions, "bigip_public_ip")
-
-	//	bigipPublicDnsName := terraform.Output(t, terraformOptions, "bigip_public_dns_name")
-
-	//	bigipUserName := terraform.Output(t, terraformOptions, "bigip_username")
-
-	//	assert.Equal(t, "azureuser", bigipUserName)
-	//	assert.Equal(t, "ecosysf5hyd2.southindia.cloudapp.azure.com", bigipPublicDnsName)
 
 }
