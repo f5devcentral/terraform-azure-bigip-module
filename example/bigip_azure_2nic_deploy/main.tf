b66dd2dc83f7e8e8a77c1371b1f29ee0964ff685
RavinderReddyF5 <r.chinthalapalli@f5.com>
2020-08-06 18:24:11 +0530

    updated example for security group changes

--- example/bigip_azure_2nic_deploy/main.tf
+++ example/bigip_azure_2nic_deploy/main.tf
@@ -67,35 +67,13 @@ data "azurerm_subnet" "external-public" {
   depends_on           = [module.network]
 }
 
-
 #
 # Create the Network Security group Module to associate with BIGIP-Mgmt-Nic
 #
 module mgmt-network-security-group {
-  source                = "Azure/network-security-group/azurerm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  security_group_name   = format("%s-mgmt-nsg-%s", var.prefix, random_id.id.hex)
-  source_address_prefix = ["10.0.1.0/24"]
-  custom_rules = [
-    {
-      name                   = "Allow_Https"
-      priority               = "200"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "443"
-      description            = "description-myhttp"
-    },
-    {
-      name                   = "allow_ssh"
-      priority               = "201"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "22"
-      description            = "Allow ssh connections"
-    }
-  ]
+  source              = "Azure/network-security-group/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  security_group_name = format("%s-mgmt-nsg-%s", var.prefix, random_id.id.hex)
   tags = {
     environment = "dev"
     costcenter  = "terraform"
@@ -105,37 +83,73 @@ module mgmt-network-security-group {
 #
 # Create the Network Security group Module to associate with BIGIP-External-Nic
 #
-module "external-network-security-group-public" {
-  source                = "Azure/network-security-group/azurerm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  security_group_name   = format("%s-external-public-nsg-%s", var.prefix, random_id.id.hex)
-  source_address_prefix = ["10.0.2.0/24"]
-  custom_rules = [
-    {
-      name                   = "myhttp"
-      priority               = "200"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "8080"
-      description            = "description-myhttp"
-    },
-    {
-      name                   = "allow_ssh"
-      priority               = "201"
-      direction              = "Inbound"
-      access                 = "Allow"
-      protocol               = "tcp"
-      destination_port_range = "22"
-      description            = "Allow ssh connections"
-    }
-  ]
+module external-network-security-group-public {
+  source              = "Azure/network-security-group/azurerm"
+  resource_group_name = azurerm_resource_group.rg.name
+  security_group_name = format("%s-external-public-nsg-%s", var.prefix, random_id.id.hex)
   tags = {
     environment = "dev"
     costcenter  = "terraform"
   }
 }
 
+resource "azurerm_network_security_rule" "mgmt_allow_https" {
+  name                        = "Allow_Https"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8443"
+  destination_address_prefix  = "*"
+  source_address_prefixes     = var.AllowedIPs
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = format("%s-mgmt-nsg-%s", var.prefix, random_id.id.hex)
+  depends_on                  = [module.mgmt-network-security-group]
+}
+resource "azurerm_network_security_rule" "mgmt_allow_ssh" {
+  name                        = "Allow_ssh"
+  priority                    = 202
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  destination_address_prefix  = "*"
+  source_address_prefixes     = var.AllowedIPs
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = format("%s-mgmt-nsg-%s", var.prefix, random_id.id.hex)
+  depends_on                  = [module.mgmt-network-security-group]
+}
+
+resource "azurerm_network_security_rule" "external_allow_https" {
+  name                        = "Allow_Https"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8443"
+  destination_address_prefix  = "*"
+  source_address_prefixes     = var.AllowedIPs
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = format("%s-external-public-nsg-%s", var.prefix, random_id.id.hex)
+  depends_on                  = [module.external-network-security-group-public]
+}
+resource "azurerm_network_security_rule" "external_allow_ssh" {
+  name                        = "Allow_ssh"
+  priority                    = 202
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  destination_address_prefix  = "*"
+  source_address_prefixes     = var.AllowedIPs
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = format("%s-external-public-nsg-%s", var.prefix, random_id.id.hex)
+  depends_on                  = [module.external-network-security-group-public]
+}
 
 
 
--- example/bigip_azure_3nic_deploy/terraform.tfvars
+++ example/bigip_azure_3nic_deploy/terraform.tfvars
@@ -1,2 +1,4 @@
 # BIG-IP Environment
 location = "eastus"
+
+AllowedIPs = ["10.20.1.0", "10.20.2.0"]
