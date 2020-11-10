18e53c3e410a4ecaa81bf9a685c0a0f0f027712c
dinesh <d.papineni@f5.com>
2020-11-10 16:11:30 +0530

    Added static IP allocation support

--- examples/bigip_azure_2nic_deploy/main.tf
+++ examples/bigip_azure_2nic_deploy/main.tf
@@ -26,9 +26,9 @@ module bigip {
   source                     = "../../"
   prefix                     = format("%s-2nic", var.prefix)
   resource_group_name        = azurerm_resource_group.rg.name
-  mgmt_subnet_ids            = [{ "subnet_id" = data.azurerm_subnet.mgmt.id, "public_ip" = true }]
+  mgmt_subnet_ids            = [{ "subnet_id" = data.azurerm_subnet.mgmt.id, "public_ip" = true, "private_ip_primary" =  "10.2.1.5"}]
   mgmt_securitygroup_ids     = [module.mgmt-network-security-group.network_security_group_id]
-  external_subnet_ids        = [{ "subnet_id" = data.azurerm_subnet.external-public.id, "public_ip" = true }]
+  external_subnet_ids        = [{ "subnet_id" = data.azurerm_subnet.external-public.id, "public_ip" = true,"private_ip_primary" = "10.2.2.40", "private_ip_secondary" = "10.2.2.50"}]
   external_securitygroup_ids = [module.external-network-security-group-public.network_security_group_id]
   availabilityZones          = var.availabilityZones
 }
